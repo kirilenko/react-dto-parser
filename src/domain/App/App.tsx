dc70e57377c4823528985e69b612b15be7385057
Nikolay Kirilenko <n@kirilenko.com>
2022-07-14 19:43:32 +0400

    feat: create empty react app

---
+++ src/domain/App/App.tsx
@@ -0,0 +1,21 @@
+import { FC } from 'react'
+import cn from 'classnames'
+
+import env from 'constants/env'
+import withRenderingTest from 'hocs/withRenderingTest'
+import log from 'utils/log'
+
+import { StyledApp } from './App.style'
+
+const App: FC = () => {
+  log('App.render')()
+
+  return (
+    <StyledApp className={cn({ __App__: env.debugClassNameEnabled })}>
+      <h1>App</h1>
+    </StyledApp>
+  )
+}
+
+export const testContent = 'App test content'
+export default withRenderingTest(App, testContent)
